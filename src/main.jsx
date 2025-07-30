8a9a7a261fa0e22440188e42d72a0530c31585e6
datnk <datnk>
2025-07-30 10:25:15 +0700

    update path policy

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/theappversal/policy" element={<PolicyPage />} />
+        <Route path="/policy" element={<PolicyPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
